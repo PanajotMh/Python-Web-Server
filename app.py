3c6c921fe2f7fb36fa10bd2dacf0b9b5ddb37980
kspiropali <kspiropali@hotmail.com>
2023-08-27 19:32:00 +0100

    broke stuff

--- app.py
+++ app.py
@@ -1,33 +1,11 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session, get_flashed_messages
+from flask import flash, Flask, render_template, request, redirect, url_for, flash, session, get_flashed_messages
 import random
 import string
 from datetime import datetime
-from google_recaptcha import ReCaptcha
-
 
 app = Flask(__name__)
 
-
-
-
-recaptcha = ReCaptcha(
-    app=app,
-    site_key="6LexhtwnAAAAAKxgTX9EcIfYsyMv8AjGtn5sQg_P",
-    secret_key="6LexhtwnAAAAAEvjrmldoFje2Y6OdpNk9c0tV2jb"
-)
-
-@app.route("/contact/", methods=["GET", "POST"])
-def home():
-
-    if recaptcha.verify():
-        print('Recaptcha has successded.')
-    else:
-        print('Recaptcha has failed.')
-
-
-
-# Mock user credentials 
-
+# Mock user credentials
 users = {
     'admin': 'password',
     'user1': 'pass123',
@@ -46,18 +24,15 @@ def home():
 
 
 @app.route("/studentCreate", methods=["POST"])
-
 def studentCreate():
     name = request.form.get("name")
     surname = request.form.get("surname")
     age = request.form.get("age")
     joiningYear = request.form.get("joiningYear")
 
-    
-
     Student = create_student(name, surname, age, joiningYear)
 
-    #Missing student ID and is not incorporated with class yet
+    # Missing student ID and is not incorporated with class yet
     Students.append({
         "name": Student.name,
         "surname": Student.surname,
@@ -65,10 +40,8 @@ def studentCreate():
         "joiningYear": Student.joingYear
     })
 
-
     response_message = f"Received: {name} {surname}, Age: {age}, Joining Year: {joiningYear}"
     return response_message, Students
-
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -85,7 +58,6 @@ def login():
             flash('Invalid username or password!', 'error')
     return render_template('login.html')
 
-from flask import flash
 
 @app.route('/logout')
 def logout():
@@ -99,10 +71,8 @@ def logout():
     flash('Logged out successfully!', 'success')  # Add flash message for successful logout
     return redirect(url_for('login'))
 
-    
 
-
-#student
+# student
 class Student:
     def __init__(self, name, surname, age, joiningYear):
         self.student_id = self.generate_student_id(name)
@@ -111,12 +81,13 @@ class Student:
         self.age = age
         self.joiningYear = joiningYear
 
-        
+
 def name_checker(name):
     if name.isalpha():
         return True
     else:
         return False
+
 
 def number_checker(age):
     try:
@@ -125,33 +96,34 @@ def number_checker(age):
     except ValueError:
         return False
 
+
 def generate_student_id(self, name):
     random_suffix = ''.join(random.choices(string.digits, k=4))
     name_without_spaces = name.replace(" ", "").lower()
     student_id = name_without_spaces[:3] + random_suffix
     return student_id
 
-def create_student(name,surname,age,joiningYear):
-        while not name_checker(name) or len(name) > 15:
-            invalid_Name = "Invalid input. Please enter letters only and a maximum of 15 characters."
-            name = input("Enter student's name: ")
 
-        while not name_checker(surname) or len(surname) > 15:
-            print("Invalid input. Please enter letters only and a maximum of 15 characters.")
-            surname = input("Enter student's surname: ")
+def create_student(name, surname, age, joiningYear):
+    while not name_checker(name) or len(name) > 15:
+        invalid_Name = "Invalid input. Please enter letters only and a maximum of 15 characters."
+        name = input("Enter student's name: ")
 
-        while not number_checker(age) or not 11 <= int(age) <= 17:
-            print("Invalid input. Please enter numbers only between 11 and 17.")
-            age = input("Enter student's age: ")
+    while not name_checker(surname) or len(surname) > 15:
+        print("Invalid input. Please enter letters only and a maximum of 15 characters.")
+        surname = input("Enter student's surname: ")
 
-        while True:
-            try:
-                joiningYear = datetime.strptime(joiningYear, "%Y-%m-%d").date()
-                break
-            except ValueError:
-                print("Invalid date format. Please enter the date in the specified format.")
-                joiningYear = input("Enter school start date (YYYY-MM-DD): ")
+    while not number_checker(age) or not 11 <= int(age) <= 17:
+        print("Invalid input. Please enter numbers only between 11 and 17.")
+        age = input("Enter student's age: ")
 
+    while True:
+        try:
+            joiningYear = datetime.strptime(joiningYear, "%Y-%m-%d").date()
+            break
+        except ValueError:
+            print("Invalid date format. Please enter the date in the specified format.")
+            joiningYear = input("Enter school start date (YYYY-MM-DD): ")
 
 
 if __name__ == '__main__':
@@ -159,4 +131,3 @@ if __name__ == '__main__':
 
 # if __name__ == '__main__':
 #     app.run(ssl_context=('certificate.pem', 'private_key.pem'), host='0.0.0.0', port=443)
-
